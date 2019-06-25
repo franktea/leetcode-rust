2e787b59a953029fc76c108151e333fe358b46c5
franktang <could.net@gmail.com>
2019-06-25 20:23:48 +0800

    2

--- src/0876_Middle_of_th_Linked_List.rs
+++ src/0876_Middle_of_th_Linked_List.rs
@@ -3,7 +3,7 @@ pub struct ListNode {
     pub val: i32,
     pub next: Option<Box<ListNode>>
 }
- 
+
 impl ListNode {
     #[inline]
     fn new(val: i32) -> Self {
@@ -26,20 +26,21 @@ impl Solution {
     }
 }
 
+
 pub struct Solution;
 
-fn to_list(arr: &[i32]) -> Option<Box<ListNode>> {
+fn vec_to_list(v: Vec<i32>) -> Option<Box<ListNode>> {
     let mut head = None;
-    for i in arr {
-        let mut new_node = ListNode::new(*i);
-        new_node.next = head;
-        head = Some(Box::new(new_node));
+    for i in v {
+        let mut node = ListNode::new(i);
+        node.next = head;
+        head = Some(Box::new(node));
     }
     head
 }
 
 fn main() {
-    let node = to_list(&[0, 1, 2, 3, 4, 5, 6]);
-    let mid = Solution::middle_node(node);
-    println!("mid val is: {}", mid.as_ref().unwrap().val);
+    let head = vec_to_list(vec![1i32, 2, 3, 4, 5, 6]);
+    let mid = Solution::middle_node(head);
+    println!("val = {}", mid.unwrap().val);
 }
