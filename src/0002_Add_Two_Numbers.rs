0d174c18a108a8318e49044314a413d8aa679998
franktang <could.net@gmail.com>
2019-07-22 17:58:56 +0800

    tmp

--- src/0002_Add_Two_Numbers.rs
+++ src/0002_Add_Two_Numbers.rs
@@ -1,41 +1,33 @@
-use std::collections::VecDeque;
-
-fn list_to_stack(mut list: Option<Box<ListNode>>) -> VecDeque<Box<ListNode>> {
-    let mut stack = VecDeque::new();
-    while let Some(mut node) = list.take() {
-        list = node.next.take();
-        stack.push_front(node);
-    }
-    stack
-}
-
 impl Solution {
     pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut stack1 = list_to_stack(l1);
-        let mut stack2 = list_to_stack(l2);
+        let mut l1 = l1;
+        let mut l2 = l2;
         let mut head = None;
+        
         fn add_node(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
             let mut n = ListNode::new(val);
             n.next = head;
             Some(Box::new(n))
         }
+        
         let mut left = 0;
+        
         loop {
-            let node1 = stack1.pop_front();
-            let node2 = stack2.pop_front();
-            if node1.is_none() && node2.is_none() {
+            if l1.is_none() && l2.is_none() {
                 if left > 0 {
-                    head = add_node(head, left);
+                   head = add_node(head, left);
                 }
-                break;
+                break; 
             }
             
             let mut sum = left;
-            if let Some(n) = node1 {
+            if let Some(n) = l1 {
                 sum += n.val;
+                l1 = n.next;
             }
-            if let Some(n) = node2 {
+            if let Some(n) = l2 {
                 sum += n.val;
+                l2 = n.next;
             }
             left = sum / 10;
             head = add_node(head, sum % 10);
