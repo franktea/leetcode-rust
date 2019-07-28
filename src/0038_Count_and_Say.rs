cc916fae05b1094c84eaac5d4e990bd3062d03a3
frank <could.net@gmail.com>
2019-07-28 20:30:49 +0800

    simplized

--- src/0038_Count_and_Say.rs
+++ src/0038_Count_and_Say.rs
@@ -2,31 +2,19 @@ impl Solution {
     pub fn count_and_say(n: i32) -> String {
         let mut s = "1".to_string();
         let mut v:Vec<String> = vec![s.clone()];
-        for _ in 0..31 {
+        for _ in 0..n {
             let mut ret = "".to_string();
-            let mut cur = None;
             let mut count = 0;
-            for c in s.chars() {
-                match cur {
-                    Some(pre) => {
-                        if c == pre {
-                            count += 1;
-                        } else {
-                            ret.push_str(&count.to_string());
-                            ret.push(pre);
-                            cur = Some(c);
-                            count = 1;
-                        }
-                    }
-                    None => {
-                        cur = Some(c);
-                        count = 1;
+            let mut it = s.chars().peekable(); // use peekable to check next char
+            while let Some(c) = it.next() {
+                match it.peek() {
+                    Some(next) if next == &c => count += 1,
+                    _ => {
+                        ret.push_str(&(count+1).to_string());
+                        ret.push(c);
+                        count = 0;
                     }
                 }
-            }
-            if let Some(pre) = cur {
-                ret.push_str(&count.to_string());
-                ret.push(pre);
             }
             v.push(ret.clone());
             s = ret;
