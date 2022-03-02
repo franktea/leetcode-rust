8eb76d988ab73b6c2cc49c869943b03492a32123
franktee <could.net@gmail.com>
2022-03-02 10:11:50 +0800

    9 redo

--- src/bin/0009.rs
+++ src/bin/0009.rs
@@ -1,14 +1,7 @@
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
-        let s = x.to_string();
-        let arr = s.as_bytes();
-        for (c1, c2) in (0..arr.len()/2).into_iter().zip(
-            (arr.len()/2..arr.len()).rev().into_iter()) {
-            if arr[c1] != arr[c2] {
-                return false;
-            }        
-        }
-        true
+        let v: Vec<_> = x.to_string().chars().collect();
+        return v.iter().rev().take(v.len()/2).eq(v.iter().take(v.len()/2));
     }
 }
 
