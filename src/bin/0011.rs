33b230a784a70f5dcb27c519cf6d117a67e87077
franktee <could.net@gmail.com>
2022-03-02 14:57:49 +0800

    11

--- src/bin/0011.rs
+++ src/bin/0011.rs
@@ -1,13 +1,17 @@
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
+        use std::cmp::{min, max};
         let mut ret = 0;
-        use std::cmp::{max, min};
         for i in 0..height.len() {
-            for j in i+1..height.len() {
+            for j in (i+1..height.len()).rev() {
+                if (j-i) as i32 * height[i] < ret {
+                    break;
+                }
                 let area = (j-i) as i32 * min(height[i], height[j]);
-                ret = max(ret, area);
+                ret = max(area, ret);
             }
         }
+
         ret
     }
 }
@@ -17,5 +21,8 @@ impl Solution {
 pub struct Solution;
 
 fn main() {
+    println!("{}", Solution::max_area(vec![1,8,6,2,5,4,8,3,7]));
+    println!("{}", Solution::max_area(vec![1,2,1]));
+    println!("{}", Solution::max_area(vec![1,2,4,3]));
     println!("{}", Solution::max_area(vec![1,8,6,2,5,4,8,3,7]));
 }
